79d8e016879c68524f098fc9401a8cbf215634f6
Sherif-lotfy <s1h9e7r4if@gmail.com>
2024-07-27 18:44:21 +0300

    creating the nav menus

---
+++ themes/fake.apple.theme/functions.php
@@ -0,0 +1,10 @@
+<?php
+add_theme_support('menus');
+
+register_nav_menus( 
+    array(
+        'main-nav' => 'main navigation menu',
+        'footer-nav' => 'Footer nav'
+        'sec-nav' => 'sec nav'
+    )
+);
